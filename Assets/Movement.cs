a17831daf65db9f0c0e37e10dc562104aa13664f
Matt Chapman <matt@mattchapman.io>
2022-04-06 22:16:59 +1200

    Work on throttle mechanic

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,41 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     private Rigidbody _rigidBody;
+    private float _throttleSetting = 0f;
+    
+    [SerializeField] private float _thrustFactor = 10;
+    [SerializeField] private float _throttleFactor = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKey(KeyCode.LeftArrow))
-       {
-           Debug.Log("Pressed left");
-           _rigidBody.AddRelativeForce(Vector3.left * 5f);
-       } 
-       if (Input.GetKey(KeyCode.RightArrow))
-       {
-           Debug.Log("Pressed right");
-           _rigidBody.AddRelativeForce(Vector3.right * 5f);
-       } 
-       if (Input.GetKey(KeyCode.UpArrow))
-       {
-           Debug.Log("Pressed right");
-           _rigidBody.AddRelativeForce(Vector3.forward * 5f);
-       } 
-       if (Input.GetKey(KeyCode.DownArrow))
-       {
-           Debug.Log("Pressed right");
-           _rigidBody.AddRelativeForce(Vector3.back * 5f);
-       } 
+        HandleThrottleChange();
+        // TODO - The plane should accelerate based on throttle. Current velocity / inertia needs to be taken into account
+        // Throttle should not control force directly
+        ApplyThrust();
+        // Debug.Log($"Velocity: {_rigidBody.velocity}");
+        Debug.Log($"Throttle Setting: {_throttleSetting}");
+        
+        
+        var inertia = _rigidBody.inertiaTensor;
+        Debug.Log($"Inertia: {inertia}");
     }
+
+    private void HandleThrottleChange()
+    {
+        if (Input.GetKey(KeyCode.W))
+            IncreaseThrottle();
+        
+        if (Input.GetKey(KeyCode.S))
+            DecreaseThrottle();
+    }
+
+    private void DecreaseThrottle()
+    {
+        var throttleCandidate = _throttleSetting -= _throttleFactor;
+
+        _throttleSetting =  throttleCandidate > 0 ? throttleCandidate : 0f;
+    }
+
+    private void IncreaseThrottle()
+    {
+        var throttleCandidate = _throttleSetting += _throttleFactor;
+
+        _throttleSetting =  throttleCandidate < 1 ? throttleCandidate : 1f;
+    }
+    
+    private void ApplyThrust()
+    {
+        var thrust = _throttleSetting * _thrustFactor;
+        Debug.Log($"Applying thrust of {thrust}");
+        
+        _rigidBody.AddRelativeForce(Vector3.forward * thrust);
+    }
+
 }
